f985e73568b879cec54b40aac10d66720e634bb8
cockmake <2359016829@qq.com>
2023-07-21 21:51:42 +0800

    开始学习第二章

--- Chapter2.cpp
+++ Chapter2.cpp
@@ -1,6 +1,16 @@
 #include<iostream>
 using namespace std;
-
 int main() {
+	/*
+	* 1. 泛型 Lambda即lambda函数支持auto形参
+	*/
+	////1.
+	//auto add = [](auto x, auto y) {
+	//	return x + y;
+	//};
+	//auto a = add(1, 2.1);
+	//cout << a << endl;
+
+
 	return 0;
 }
